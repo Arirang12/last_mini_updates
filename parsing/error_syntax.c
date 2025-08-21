6be874cbe03ffb11e7db958acd1a338dcb93e756
Zaboub Hassna <zhassna@c3r6p1.1337.ma>
2025-08-21 03:25:23 +0100

    some_fixed

---
+++ parsing/error_syntax.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_syntax.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhassna <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 04:53:27 by zhassna           #+#    #+#             */
+/*   Updated: 2025/08/21 02:56:25 by zhassna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+int	check_for_syntax_error(t_token *token)
+{
+	if (!token)
+		return (0);
+	while (token->next)
+	{
+		if (token->type != WORD && (token->type == token->next->type))
+		{
+			write(2, "Error! syntax error near unexpected token `newline'\n",
+				53);
+			g_exit = 2;
+			return (1);
+		}
+		token = token->next;
+	}
+	if (token->type != WORD)
+	{
+		write(2, "Error! syntax error near unexpected token `newline'\n", 53);
+		g_exit = 2;
+		return (1);
+	}
+	return (0);
+}
+
+void	is_heredoc(t_token *tokens, const char *line, t_env *env)
+{
+	int	t;
+
+	t = 0;
+	while (tokens)
+	{
+		if (tokens && tokens->type == HEREDOC && tokens->next == NULL)
+		{
+			env->heredoc = true;
+			while (line[t])
+			{
+				if (line[t] == '\'' || line[t] == '\"')
+				{
+					tokens->expand = false;
+					break ;
+				}
+				t++;
+			}
+		}
+		tokens = tokens->next;
+	}
+}
